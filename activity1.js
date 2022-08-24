6e5b4278837f60159b92f7b3af7b7cbd59ca70ef
Vlad <vladut_stoian@live.co.uk>
2022-08-24 17:26:41 +0100

    Initial Push

---
+++ activity1.js
@@ -0,0 +1,14 @@
+//ACITIVTY1 ----------
+
+const greeting = "Hello Codenation"
+const runs = 5
+
+function doGreeting()
+{
+    for (let index = 0; index < 5; index++)
+    {
+        console.log(greeting)
+    }
+}
+
+doGreeting()
---
+++ activity2.js
@@ -0,0 +1,11 @@
+// //ACTIVITY2 --------------
+const array = [1, 5, 6, 7, 3]
+
+
+
+function useMap()
+{
+    array.map((array) => console.log(array * 3))
+}
+
+useMap()
